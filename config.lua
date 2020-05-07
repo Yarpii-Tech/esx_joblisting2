09db185f28da678eafdceb5c324a91ae7783e232
Jeffrey Stendert <37269702+Yarpii-Tech@users.noreply.github.com>
2020-05-08 00:53:40 +0200

    maaaaybe

--- config.lua
+++ config.lua
@@ -1,14 +1,10 @@
-Config              = {}
-Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 2.0, y = 2.0, z = 2.0}
-Config.MarkerColor  = {r = 0, g = 255, b = 0}
-Config.MarkerType   = 27
-Config.Locale = 'en'
-
-Config.Zones = {
-	{x = -268.8589, y = -956.7532, z = 30.223}
-}
-
+Config             	= {}
+Config.DrawDistance 	= 100.0
+Config.ZoneSize     	= {x = 2.0, y = 2.0, z = 2.0}
+Config.MarkerColor  	= {r = 0, g = 255, b = 0}
+Config.MarkerType   	= 27
+Config.Locale 		= 'en'
+Config.Zones 		= {{x = -268.8589, y = -956.7532, z = 30.223}}
 Config.Webhook = {
 	police = "police",
 	ambulance = "ambulance",
