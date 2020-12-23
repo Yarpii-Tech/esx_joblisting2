1586511df92bce10f3e213360a782cfb01d8ac7e
Jeffrey Stendert <37269702+Yarpii@users.noreply.github.com>
2020-12-23 23:22:01 +0100

    .

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,28 @@
-fx_version'bodacious'
-game'gta5'
+fx_version 'cerulean'
+game 'gta5'
 
 client_scripts {
-'@es_extended/locale.lua',
-'config.lua',
-'locales/en.lua',
-'client/esx_joblisting_cl.lua'
+  '@es_extended/locale.lua',
+  'config.lua',
+  'locales/en.lua',
+  'client/esx_joblisting_cl.lua'
 }
+
 server_scripts {
-'@mysql-async/lib/MySQL.lua',
-'@es_extended/locale.lua',
-'config.lua',
-'locales/en.lua',
-'server/esx_joblisting_sv.lua'
+  '@mysql-async/lib/MySQL.lua',
+  '@es_extended/locale.lua',
+  'config.lua',
+  'locales/en.lua',
+  'server/esx_joblisting_sv.lua'
 }
+
 files {
-'html/ui.html',
-'html/styles.css',
-'html/scripts.js',
-'html/debounce.min.js',
-'html/img/header.png',
-'html/img/tablet.png'
+  'html/ui.html',
+  'html/styles.css',
+  'html/scripts.js',
+  'html/debounce.min.js',
+  'html/img/header.png',
+  'html/img/tablet.png',
 }
+
 ui_page 'html/ui.html'
